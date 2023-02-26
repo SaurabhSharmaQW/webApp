ba1acb2305d8eb83804c61a6f602f25be5829100
Saurabh Sharma <sharmasaurabh2412@gmail.com>
2023-02-26 13:55:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var slideIndex = 1;
+var timer = null;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  clearTimeout(timer);
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  clearTimeout(timer);
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n == undefined) {
+    n = ++slideIndex;
+  }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  timer = setTimeout(showSlides, 5000);
+}
+
+let theme = localStorage.getItem("theme");
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (let index = 0; index < themeDots.length; index++) {
+  themeDots[index].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById("theme-style").href = "index.css";
+  }
+  if (mode == "blue") {
+    document.getElementById("theme-style").href = "blue.css";
+  }
+  if (mode == "green") {
+    document.getElementById("theme-style").href = "green.css";
+  }
+  if (mode == "purple") {
+    document.getElementById("theme-style").href = "purple.css";
+  }
+  localStorage.setItem("theme", mode);
+}
